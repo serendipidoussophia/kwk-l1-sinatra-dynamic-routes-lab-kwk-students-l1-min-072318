cc417d29a9de9ef6db83221a1cc055bd59924de0
serendipidoussophia <scurranmoore@gmail.com>
2018-07-31 19:18:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ class App < Sinatra::Base
     "#{@user_name.reverse}"
   end
   get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number}"
   # get '/say/:number/:phrase' do
   #   @number = params[:number].to_i
   #   @phrase = params[:phrase]
