ac46fd4a14a0a3d94d241eb4a5c603ded3dab6f1
serendipidoussophia <scurranmoore@gmail.com>
2018-07-31 20:13:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,13 +18,14 @@ class App < Sinatra::Base
     final_string
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-  @word1 = params[:word1]
-  @word2 = params[:word2]
-  @word3 = params[:word3]
-  @word4 = params[:word4]
-  @word5 = params[:word5]
-  "#{params[:@word1]} #{params[:@word2]} #{params[:@word3]} #{params[:@word4]} #{params[:@word5]}."
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    "#{params[:@word1]} #{params[:@word2]} #{params[:@word3]} #{params[:@word4]} #{params[:@word5]}."
   end
   get '/:operation/:number1/:number2'
-  @operation = params[:operation]
+  
+  end
 end
