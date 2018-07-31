b86ef88d98ab124aa9431be5b4b5830921395941
serendipidoussophia <scurranmoore@gmail.com>
2018-07-31 19:52:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,23 +9,21 @@ class App < Sinatra::Base
   get '/square/:number' do
     num1 = params[:number]
     (num1.to_i**2).to_s)
-    end
   end
   # get '/say/:number/:phrase' do
   #   @number = params[:number].to_i
   #   @phrase = params[:phrase]
   #   "#{@phrase}"
   # end
-end
-get '/say/:word1/:word3/:word4/:word5' do
+  end
+  get '/say/:word1/:word3/:word4/:word5' do
   @word1 = params[:word1]
   @word2 = params[:word2]
   @word3 = params[:word3]
   @word4 = params[:word4]
   @word5 = params[:word5]
   "#{@word1 @word2 @word3 @word4 @word5}"
-end
-get '/:operation/:number1/:number2'
+  end
+  get '/:operation/:number1/:number2'
   @operation = params[:operation]
-  
 end
