edbcfb00811ae254e446098c43c09e15d1e429fc
serendipidoussophia <scurranmoore@gmail.com>
2018-07-31 20:17:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,5 +28,8 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @operation = params[:operation]
     @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    if @operation == "multiply"
+      (@number1*@number2).to_s
   end
 end
